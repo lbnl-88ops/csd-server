be815cb6fc8e2dd79b0b3c54cdf5375548416c82
Jessica Rehak <rehak@lbl.gov>
2026-01-13 06:32:49 -0800

    added flask configuration

--- src/csd_server/cli.py
+++ src/csd_server/cli.py
@@ -1,19 +1,59 @@
+from dataclasses import dataclass
 from pathlib import Path
+from typing import List
 from typing_extensions import Annotated
 
+from flask import Flask, jsonify, send_file
 import typer
 from rich import print, style
 
-app = typer.Typer(help="Charge state distribute file server.", no_args_is_help=True)
+typer_app = typer.Typer(
+    help="Charge state distribute file server.", no_args_is_help=True
+)
+app = Flask(__name__)
 
 
-@app.command()
-def main(csd_directory: Annotated[Path, typer.Argument(help="Directory to serve")]):
+@dataclass(init=False)
+class ServerState:
+    directory: Path
+
+
+state = ServerState()
+
+
+def list_files(directory: Path) -> List[Path]:
+    files = list(directory.glob("csd_*"))
+    return files
+
+
+@app.route("/files", methods=["GET"])
+def files():
+    return jsonify([str(f) for f in list_files(state.directory)])
+
+
+@app.route("/download/<filename>", methods=["GET"])
+def download_file(filename):
+    file_path = state.directory / filename
+    if file_path.exists():
+        return send_file(file_path, as_attachment=True)
+    else:
+        return jsonify({"error": "File not found"}), 404
+
+
+@typer_app.command()
+def main(
+    csd_directory: Annotated[Path, typer.Argument(help="Directory to serve")],
+    port: Annotated[int, typer.Option(help="Port to use")] = 5000,
+):
     if not csd_directory.exists():
         print(f"[red]Directory {csd_directory.absolute()} does not exist[/red]")
         raise typer.Abort()
     print(f"Serving files from {csd_directory.absolute()}")
+    files = list_files(csd_directory)
+    print(f"Serving [bold]{len(files)}[/bold] CSD files")
+    state.directory = csd_directory.absolute()
+    app.run("0.0.0.0", port)
 
 
 if __name__ == "__main__":
-    app()
+    typer_app()
